5f4be54cba41d296c31c210bb601a36902aa5ec3
Blade Lucas <elffe2@hotmail.ca>
2023-01-29 20:58:35 -0500

    Updated files

--- scripts/main.js
+++ scripts/main.js
@@ -7,15 +7,30 @@
         })
 
         let mainContent = document.getElementsByTagName("main")[0]
+        //mainContent.setAttribute("class", "container")
         
+        //documentBody = document.body
+
         let mainParagraph = document.createElement("p")
         mainParagraph.setAttribute("id", "MainParagraph")
         mainParagraph.setAttribute("class", "mt-3")
 
-        let firstString = "This is a main paragraph that we added through javascript"
-        mainParagraph.textContent = firstString
+        let firstString = "This is a "
+        let secondString = `${ firstString } main paragraph that we added through javascript`
+        mainParagraph.textContent = secondString
         
+        //mainContent.before(mainParagraph)
+
         mainContent.appendChild(mainParagraph)
+
+        //document.getElementById("RandomButton").remove()
+
+        // documentBody.innerHTML = `
+        //     <div class="container">
+        //         <h1 class="display-1">Hello WEBD6201</h1>
+        //         <p class="mt-5 lead">and... what do you this of this method?</p>
+        //     </div>
+        // `
     }
 
     function Start() {
